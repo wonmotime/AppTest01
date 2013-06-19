b96e3945cf9d05e0d459c50efc4a998ba4cf9856
Wonjae Park <wonjaep@hotmail.com>
2013-06-19 19:24:44 +0900

    firstcommit
    Generated first app

---
+++ ClubGAZAapp/ClubGAZAappTests/ClubGAZAappTests.h
@@ -0,0 +1,13 @@
+//
+//  ClubGAZAappTests.h
+//  ClubGAZAappTests
+//
+//  Created by Wonjae Park on 6/19/13.
+//  Copyright (c) 2013 starindex. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ClubGAZAappTests : SenTestCase
+
+@end
---
+++ ClubGAZAapp/ClubGAZAappTests/ClubGAZAappTests.m
@@ -0,0 +1,32 @@
+//
+//  ClubGAZAappTests.m
+//  ClubGAZAappTests
+//
+//  Created by Wonjae Park on 6/19/13.
+//  Copyright (c) 2013 starindex. All rights reserved.
+//
+
+#import "ClubGAZAappTests.h"
+
+@implementation ClubGAZAappTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ClubGAZAappTests");
+}
+
+@end
